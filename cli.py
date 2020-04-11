846097447c568ad69724e6bb4e4945a31f28cc23
timwis <tim@timwis.com>
2020-04-11 14:34:52 +0100

    Add prepare-calls command

--- cli.py
+++ cli.py
@@ -1,22 +1,25 @@
 import json
 from datetime import datetime
+import pdb
 
 import click
 import petl as etl
 
+@click.group()
+def cli():
+  pass
 
-@click.command()
+@cli.command()
 @click.argument('gds_file_path')
 @click.argument('healthintent_file_path')
 def prepare_contacts(gds_file_path, healthintent_file_path):
   """Extracts core contact fields from gds_file_path, and adds a serialized
      version of the records from each file as json columns."""
-  gds_table = etl.fromcsv(gds_file_path)
-  gds_header = gds_table.fieldnames()
+
   now = datetime.now().isoformat()
 
-  gds_table = gds_table \
-    .addfield('gds_import_data', serialize_row(gds_header)) \
+  gds_table = etl.fromcsv(gds_file_path) \
+    .addfield('gds_import_data', serialize_row) \
     .addfield('created_at', now) \
     .addfield('updated_at', now) \
     .addfield('address', concat_address) \
@@ -43,19 +46,35 @@ def prepare_contacts(gds_file_path, healthintent_file_path):
          'updated_at',
          'gds_import_data')
 
-  healthintent_table = etl.fromcsv(healthintent_file_path)
-  healthintent_header = healthintent_table.fieldnames()
-
-  healthintent_table = healthintent_table \
-    .addfield('healthintent_import_data', serialize_row(healthintent_header)) \
+  healthintent_table = etl.fromcsv(healthintent_file_path) \
+    .addfield('healthintent_import_data', serialize_row) \
     .rename('NHS number', 'nhs_number') \
     .cut('nhs_number', 'healthintent_import_data')
 
   etl.join(gds_table, healthintent_table, key='nhs_number') \
     .tocsv()
 
-def serialize_row(keys):
-  return lambda row: json.dumps(dict(zip(keys, row)))
+@cli.command()
+@click.argument('calls_file_path')
+def prepare_calls(calls_file_path):
+  """Prepares call log records for import into a temporary calls table"""
+
+  # Expected file is in 'windows-1252' file encoding
+  etl.fromcsv(calls_file_path, encoding='windows-1252') \
+    .addfield('import_data', serialize_row) \
+    .addfield('body', compose_body) \
+    .rename({'Shielded ID': 'shielded_id',
+             'Contact attempted (date)': 'latest_attempt_date'}) \
+    .convert('latest_attempt_date', parse_date) \
+    .cut('shielded_id',
+         'latest_attempt_date',
+         'body',
+         'import_data') \
+    .tocsv()
+
+def serialize_row(row):
+  keys = row.flds # Note: flds is an undocumented property
+  return json.dumps(dict(zip(keys, row)))
 
 # Concatenate non-empty address parts with ', ' separator
 def concat_address(row):
@@ -79,5 +98,12 @@ def add_leading_zero_if_missing(value):
   else:
     return value
 
+def compose_body(row):
+  keys_to_omit = ['Shielded ID', 'Contact attempted (date)', 'Time', 'import_data']
+  lines = [f"{key}: {row[key]}"
+          for key in row.flds # Note: flds is an undocumented property
+          if key not in keys_to_omit and row[key]]
+  return "\n".join(lines)
+
 if __name__ == '__main__':
-  prepare_contacts()
+  cli()
