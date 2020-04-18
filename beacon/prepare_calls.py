5c3f3329b72b4f7fc23ba4e70737c26ace962fd5
timwis <tim@timwis.com>
2020-04-18 14:01:03 +0000

    Generate callback needs

--- beacon/prepare_calls.py
+++ beacon/prepare_calls.py
@@ -1,5 +1,6 @@
 import re
 import json
+from datetime import datetime, timedelta
 
 import click
 import petl as etl
@@ -9,8 +10,9 @@ from .calls_header_map import header_map, rename_map
 
 MSG_ORIGINAL_TRIAGE_NEED = '[Import]: Imported from call log spreadsheet'
 MSG_CALL_LOG_NOTE = '[Import]: Imported call log'
-MSG_IDENTIFIED_NEED = '[Import]: Need identified automatically from imported call log'
-MSG_CLOSED_FOOD_NEED = '[Import]: Marked completed because priority 1 and 2 food needs were all met by the time of data import'
+MSG_IDENTIFIED_NEED = '[Import]: Need created automatically from imported call log'
+MSG_IDENTIFIED_CALLBACK_NEED = '[Import]: Callback need created automatically because the imported call log had a food need or callback date specified'
+MSG_CLOSED_FOOD_NEED = '[Import]: Marked completed because priority 1 and 2 food needs were all met by the time the call log was imported'
 
 @click.command()
 @click.argument('calls_file_path')
@@ -64,10 +66,11 @@ def prepare_calls(calls_file_path, needs_output_file_path, notes_output_file_pat
          'updated_at',
          'import_data')
 
+  # TODO: Assign to food team?
   identified_food_needs = spreadsheet \
     .rename({'latest_attempt_date': 'created_at'}) \
     .addfield('updated_at', lambda row: row['created_at']) \
-    .selectin('outcome', ['Food referral', 'Food and Other referral']) \
+    .select(needs_food) \
     .addfield('category', 'groceries and cooked meals') \
     .convert('food_priority', parse_food_priority) \
     .addfield('supplemental_data', construct_supplemental_data) \
@@ -80,6 +83,23 @@ def prepare_calls(calls_file_path, needs_output_file_path, notes_output_file_pat
          'supplemental_data',
          'created_at',
          'updated_at')
+
+  callback_needs = spreadsheet \
+    .rename({'latest_attempt_date': 'created_at'}) \
+    .addfield('updated_at', lambda row: row['created_at']) \
+    .convert('callback_date', parse_callback_date) \
+    .select(needs_callback) \
+    .addfield('category', 'phone triage') \
+    .addfield('name', compose_callback_need_name) \
+    .addfield('start_on', determine_callback_start_date) \
+    .cut('nhs_number',
+         'category',
+         'name',
+         'created_at',
+         'updated_at',
+         'start_on').tocsv()
+
+  # TODO: 'Other' needs, 'Addl' needs, person data (e.g. dietary reqs)
 
 # Impure: Uses global header_map variable
 def compose_body(row):
@@ -137,3 +157,34 @@ def compose_food_need_name(row):
     lines.append(MSG_CLOSED_FOOD_NEED)
   lines.append(compose_body(row))
   return "\n".join(lines)
+
+def parse_callback_date(value):
+  date_like_string = re.search(r'(\d+[/\.]\d+[/\.]\d+)', value).group(1)
+  if date_like_string:
+    possible_formats = [
+      '%d/%m/%Y',
+      '%d.%m.%y'
+    ]
+    for fmt in possible_formats:
+      try:
+        return datetime.strptime(date_like_string, fmt) \
+                       .date()
+      except ValueError:
+        pass
+
+  return None
+
+def needs_food(row):
+  return row['outcome'] in ['Food', 'Food and Other referral'] \
+         or row['food_priority']
+
+def needs_callback(row):
+  return row['callback_date'] \
+         or needs_food(row) \
+         or row['book_weekly_food_delivery'] == True
+
+def determine_callback_start_date(row):
+  return row['callback_date'] or row['created_at'] + timedelta(days=6)
+
+def compose_callback_need_name(row):
+  return MSG_IDENTIFIED_CALLBACK_NEED + '\n' + compose_body(row)
